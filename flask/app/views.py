46b0387c8bcb57166297a5ff86f3f4ea2efd4f39
Yuri Pirola <yuri.pirola@gmail.com>
2020-04-09 00:14:29 +0200

    Add static files and templates

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,8 +1,13 @@
 import subprocess
+from flask import render_template
 from app import app
 
 @app.route('/')
 def home():
+   return render_template('home.html')
+
+@app.route('/snakemake')
+def snakemake():
    p = subprocess.run(
       ["/bin/bash", "-c", "-l", "snakemake -v"],
       capture_output=True,
