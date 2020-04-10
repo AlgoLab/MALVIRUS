7fa8b14b8cb266e7ed8732c4c9b75c9aa00871dc
Yuri Pirola <yuri.pirola@gmail.com>
2020-04-10 18:07:30 +0200

    FE: add refresh on single VCF page

--- frontend/src/pages/Vcf/Vcf.jsx
+++ frontend/src/pages/Vcf/Vcf.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Button } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
 
-function Vcf({ id, vcf }) {
+function Vcf({ id, vcf, reloadVcf }) {
   return (
     <>
       <h1>Reference VCF {id}</h1>
       <p>It displays the details of the reference VCF.</p>
       <pre>{JSON.stringify(vcf, null, 2)}</pre>
+      <Button type="primary" icon={<SyncOutlined />} onClick={reloadVcf}>
+        Refresh
+      </Button>
     </>
   );
 }
