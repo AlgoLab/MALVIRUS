36912f067cd1039e4095180cc7fb5bfa6abe2258
Luca Denti <ldenti92@gmail.com>
2020-04-16 19:55:59 +0200

    Adds script to clean multi-reference fasta (just to have it somewhere - it should be part of the "best-practices")

---
+++ software/bin/clean_fasta.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+from Bio import SeqIO
+
+def main():
+    fa_path = sys.argv[1]
+    seq_to_remove = sys.argv[2] # eg NC_045512.2 or EPI_ISL_416412
+    for record in SeqIO.parse(fa_path, "fasta"):
+        if seq_to_remove in record.id:
+            continue
+        nn = record.upper().seq.count("N")
+        if nn == 0:
+            SeqIO.write(record, sys.stdout, "fasta")
+
+if __name__ == "__main__":
+    main()
