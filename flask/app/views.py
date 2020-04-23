b33a94c3d7050a819fa2ab5dd06849c8606eb2e6
Yuri Pirola <yuri.pirola@gmail.com>
2020-04-23 09:57:25 +0200

    BE: remove unused code

--- flask/app/views.py
+++ flask/app/views.py
@@ -22,24 +22,9 @@ def mkdirp(path):
     Path(path).mkdir(parents=True, exist_ok=True)
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/snakemake')
-def snakemake():
-    p = subprocess.run(
-        ["/bin/bash", "-c", "-l", "snakemake -v"],
-        capture_output=True,
-        text=True
-    )
-    return p.stdout
-
-
 @app.route('/<path:route>')
 def not_found(route):
-    return 'Route ' + route + ' not found!', 404
+    return abort(make_response(jsonify(message='Route not found'), 404))
 
 
 @app.route('/vcf', methods=['GET'])
