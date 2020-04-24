7a9ec068e18f4a076f2a161b742035abf8a9cc5e
Yuri Pirola <yuri.pirola@gmail.com>
2020-04-24 15:55:58 +0200

    FE: link to the tutorial if a job table is empty

--- frontend/src/utils/tables.js
+++ frontend/src/utils/tables.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { Empty } from 'antd';
 
-import { JOB_STATUSES } from 'app-config';
 import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
+
+import { JOB_STATUSES } from 'app-config';
 
 export const jobStatusFilters = {
   filters: Object.keys(JOB_STATUSES).map((status) => ({
@@ -19,27 +21,49 @@ export const submissionTimeRender = (value) =>
 export const submissionTimeSorter = (a, b) =>
   (a.submission_time || 0) - (b.submission_time || 0);
 
+const spanStyle = { display: 'block', marginTop: '1em' };
+
 export const EmptyWithFilters = (
   <Empty
     description={
       <>
         No job found!
-        <br />
-        Some filters are active: disable them to see the full list.
+        <span style={spanStyle}>
+          Some filters are active: disable them to see the full list.
+        </span>
       </>
     }
   />
 );
 
-export const EmptyWoFilters = <Empty description="No job found!" />;
+export const EmptyWoFilters = (
+  <Empty
+    description={
+      <>
+        No job found!
+        <span style={spanStyle}>
+          <b>Submit a new job</b> with the buttons below
+          <br />
+          OR
+          <br /> have a look to the{' '}
+          <Link to="/help/TUTORIAL" target="_blank" style={{ fontWeight: 700 }}>
+            awesome tutorial
+          </Link>{' '}
+          to learn how to use MALVIRUS.
+        </span>
+      </>
+    }
+  />
+);
 
 export const NoDataWithFilters = (
   <Empty
     description={
       <>
         No Data!
-        <br />
-        Some filters are active: disable them to see the full list.
+        <span style={spanStyle}>
+          Some filters are active: disable them to see the full list.
+        </span>
       </>
     }
   />
