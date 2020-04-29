677efb7684792b0d734458e67bbc79b8dafd5027
Luca Denti <ldenti92@gmail.com>
2020-04-29 15:28:27 +0200

    Updates script to filter FASTA

--- software/bin/clean_fasta.py
+++ software/bin/clean_fasta.py
@@ -5,15 +5,27 @@ import re
 
 from Bio import SeqIO
 
-def main():
-    fa_path = sys.argv[1]
-    seq_to_remove = sys.argv[2] if len(sys.argv) > 2 else None # eg NC_045512.2 or EPI_ISL_416412
+def clean():
+    fa_path = sys.argv[2]
+    seq_to_remove = sys.argv[3] if len(sys.argv) > 3 else None # eg NC_045512.2 or EPI_ISL_416412
     for record in SeqIO.parse(fa_path, "fasta"):
         if seq_to_remove is not None and seq_to_remove in record.id:
             continue
-        nn = record.upper().seq.count("N")
-        if nn == 0:
+        SeqIO.write(record, sys.stdout, "fasta")
+
+def extract():
+    fa_path = sys.argv[2]
+    seq_to_remove = sys.argv[3]
+    for record in SeqIO.parse(fa_path, "fasta"):
+        if seq_to_remove in record.id:
             SeqIO.write(record, sys.stdout, "fasta")
+            break
+
+def main():
+    if sys.argv[1] == "clean":
+        clean()
+    elif sys.argv[1] == "extract":
+        extract()
 
 if __name__ == "__main__":
     main()
